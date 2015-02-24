493dcf38fd871965e744bc60a25205315a9dbbc2
Sebastian Perez Saaibi <saaibi.sps@gmail.com>
2015-02-24 13:46:57 -0500

    add hands on and notes on R and Linear Algebra

---
+++ hands_on/R/example-gapminder-ggplot.r
@@ -0,0 +1,25 @@
+## Example taken from JennyBc ggplot2 tutorial:
+## https://github.com/jennybc/ggplot2-tutorial/
+
+#+ setup, include = FALSE
+library(knitr)
+
+library(ggplot2)
+library(RColorBrewer)
+
+#' load data
+gdURL <- "http://tiny.cc/gapminder"
+gDat <- read.delim(file = gdURL) 
+str(gDat)
+
+#' let just look at four countries
+jCountries <- c("Canada", "Rwanda", "Cambodia", "Mexico", "Colombia")
+x <- droplevels(subset(gDat, country %in% jCountries))
+ggplot(x, aes(x = year, y = lifeExp, color = country)) +
+  geom_line() + geom_point()
+
+#' reorder the country factor to reflect lifeExp in 2007
+x <- transform(x, country = reorder(country, -1 * lifeExp, max))
+ggplot(x, aes(x = year, y = lifeExp, color = country)) +
+  geom_line() + geom_point()
+
---
+++ notes/06.LinAlg/links.md
@@ -0,0 +1,6 @@
+# Links
+
+- [Linear Algebra Cheat-Sheet, R](http://www.statmethods.net/advstats/matrix.html)
+- [Linear Algebra, R](http://blog.revolutionanalytics.com/2013/08/r-and-linear-algebra.html)
+- [Numpy Linear Algebra, Python](http://docs.scipy.org/doc/numpy/reference/routines.linalg.html)
+- [Linear Algebra, Python](http://userpages.umbc.edu/~rcampbel/Computers/Python/linalg.html)
