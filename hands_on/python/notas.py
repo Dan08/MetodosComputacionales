d66749ebe90ba280c1e7bb67e95d9c385a75943c
AdminBiocomputo <j.e.forero.romero@gmail.com>
2015-08-11 09:35:51 -0500

    nueva version separado por funciones

--- hands_on/python/notas.py
+++ hands_on/python/notas.py
@@ -1,27 +1,41 @@
 import sys
 
-infile = open(sys.argv[1], 'r')
-print("estoy leyendo %s"%(sys.argv[1]))
-outganador= open('mecanica_ganadores.txt', 'w')
-outperdedor= open('mecanica_perdedores.txt', 'w')
 
-lineas = infile.readlines()
+def load_data(filename):
+    infile = open(filename, 'r')
+    print("estoy leyendo %s"%(filename))
+    l = infile.readlines()
+    infile.close()
+    return l
 
-for linea in lineas:
-    if(linea[0]!='#'):
-        ls = linea.split()
-        nota1 = float(ls[3])
-        nota2 = float(ls[4])
-        nota3 = float(ls[5])        
-        promedio = (nota1 + nota2 + nota3)/3.0
-        if(promedio>=3.0):
-            outganador.write("%s %s %s %f\n"%(ls[0], ls[1], ls[2], promedio))
-        else:
-            outperdedor.write("%s %s %s %f\n"%(ls[0], ls[1], ls[2], promedio))
-    else:
-        outganador.write(linea)
-        outperdedor.write(linea)
+def process_data(lineas, select="ganador"):
+    nuevas_lineas = []
+    linea_pierde = ''
+    linea_gana = ''
+    for linea in lineas:
+        if(linea[0]!='#'):
+            ls = linea.split()
+            nota1 = float(ls[3])
+            nota2 = float(ls[4])
+            nota3 = float(ls[5])        
+            promedio = (nota1 + nota2 + nota3)/3.0
+            if((promedio>=3.0) & (select=="ganador")):                
+                linea_gana = "%s %s %s %f\n"%(ls[0], ls[1], ls[2], promedio)
+                nuevas_lineas.append(linea_gana)
+            elif((promedio<3.0) & (select=="perdedor")):
+                linea_pierde = "%s %s %s %f\n"%(ls[0], ls[1], ls[2], promedio)                
+                nuevas_lineas.append(linea_pierde)                
+    return nuevas_lineas
 
-outganador.close()
-outperdedor.close()
-infile.close()
+def write_data(lineas, filename):
+    outfile = open(filename, 'w')
+    print("estoy escribiendo %s"%(filename))
+    for line in lineas:
+        outfile.write(line)
+    outfile.close()
+
+
+lineas = load_data(sys.argv[1])
+nuevas_ganador = process_data(lineas,  select='ganador')
+write_data(nuevas_ganador, 'ganadores.txt')
+
