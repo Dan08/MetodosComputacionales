f53c869d66b51a6886d170ef2301e24e48ad2511
Jaime Forero <j.e.forero.romero@gmail.com>
2016-08-24 16:17:26 -0500

    without nested functions

--- hands_on/diff-integration/integral.py
+++ hands_on/diff-integration/integral.py
@@ -23,59 +23,59 @@ def exact(x):
 def error(real, calculated):
     return abs((real-calculated)/real)
 
+    
+# calculates integral using montecarlo
+def montecarlo(f, a, b, n):    
+    x = np.random.random(n)
+    f_eval = f(x)
+    result = f_eval.mean()
+    return np.log10(n), result
+
+# calculates integral using trapezoid method
+def trapezoid(f, a, b, n):
+    x = np.linspace(a, b, n+1)
+    h = (b-a)/n
+    result = 0
+    for i in range(n+1):
+        if i == 0 or i == n:
+            weight = h/2.
+        else:
+            weight = h
+        result += f(x[i])*weight
+    return np.log10(n), result
+        
+# calculates integral using Simpson method
+def simpson(f, a, b, n):
+    if n%2 == 1:
+        n += 1
+    h = (b-a)/n
+    result = f(a) + f(b)
+    for i in range(1, n, 2):
+        result += 4*f(a + i*h)
+    for i in range(2, n-1, 2):
+        result += 2*f(a + i*h)
+    result *= h/3.
+    n = np.log10(n)
+    return n, result
+            
+# calculates integral using Simpson 3/8 method
+def simpson38(f, a, b, n):
+    if n%3 != 0:
+        n += 3 - n%3            
+    h = (b-a)/n
+    result = f(a) + f(b)
+    for i in range(1, n, 3):
+        result += 3*f(a + i*h)
+    for i in range(2, n, 3):
+        result += 3*f(a + i*h)
+    for i in range(3, n, 3):
+        result += 2*f(a + i*h)
+    result *= 3*h/8.
+    
+    return np.log10(n), result
+        
 # calculates an integral
 def integrate(func, a, b, n):
-    
-    # calculates integral using montecarlo
-    def montecarlo(f, a, b, n):    
-        x = np.random.random(n)
-        f_eval = f(x)
-        result = f_eval.mean()
-        return np.log10(n), result
-
-    # calculates integral using trapezoid method
-    def trapezoid(f, a, b, n):
-        x = np.linspace(a, b, n+1)
-        h = (b-a)/n
-        result = 0
-        for i in range(n+1):
-            if i == 0 or i == n:
-                weight = h/2.
-            else:
-                weight = h
-            result += f(x[i])*weight
-        return np.log10(n), result
-        
-    # calculates integral using Simpson method
-    def simpson(f, a, b, n):
-        if n%2 == 1:
-            n += 1
-        h = (b-a)/n
-        result = f(a) + f(b)
-        for i in range(1, n, 2):
-            result += 4*f(a + i*h)
-        for i in range(2, n-1, 2):
-            result += 2*f(a + i*h)
-        result *= h/3.
-        n = np.log10(n)
-        return n, result
-            
-    # calculates integral using Simpson 3/8 method
-    def simpson38(f, a, b, n):
-        if n%3 != 0:
-            n += 3 - n%3            
-        h = (b-a)/n
-        result = f(a) + f(b)
-        for i in range(1, n, 3):
-            result += 3*f(a + i*h)
-        for i in range(2, n, 3):
-            result += 3*f(a + i*h)
-        for i in range(3, n, 3):
-            result += 2*f(a + i*h)
-        result *= 3*h/8.
-        
-        return np.log10(n), result
-        
     # calls functions
     montecarlo_results = montecarlo(func, a, b, n)
     trapezoid_results = trapezoid(func, a, b, n)
@@ -87,6 +87,7 @@ def integrate(func, a, b, n):
     numbers = [item[0] for item in results]
     results = [np.log10(error(exact_value, item[1])) for item in results]
     return numbers, results
+
 
 exact_value = exact(upper_limit) - exact(lower_limit)
 points = 20
