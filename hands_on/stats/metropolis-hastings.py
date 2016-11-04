2584f93920ef3559ed785396bc6b74e0d0576b48
Juan Barbosa <js.barbosa10@uniandes.edu.co>
2016-11-04 16:16:25 -0500

    Add files via upload

---
+++ hands_on/stats/metropolis-hastings.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov  4 16:05:24 2016
+
+@author: juan
+"""
+
+import numpy as np
+from numpy.random import random
+import matplotlib.pyplot as plt
+
+N = int(1e5)
+
+x = np.zeros(N)
+
+delta = 0.2
+sigma = 1
+
+x[0] = random()
+
+def gaussian(x):
+    return np.exp(-x**2/(2*sigma**2))/(sigma * np.sqrt(2*np.pi))
+
+for i in range(1, N):
+    x_new = x[i-1] + delta*(random()-0.5)*2
+    alpha = min(1, gaussian(x_new)/gaussian(x[i-1]))
+    
+    u = random()
+    if (u < alpha):
+        x[i] = x_new
+    else:
+        x[i] = x[i-1]
+        
+plt.hist(x, bins = 50, normed = True)
+plt.show()
